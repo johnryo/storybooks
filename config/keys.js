ad0dfc19c8eed0758d158dfb84b1060eaacc0a6b
John <jpogge55@gmail.com>
2018-12-09 16:03:27 -0800

    Handled auth

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:'mongodb://johno:sugoi101@ds029267.mlab.com:29267/storybooks-dev',
+  googleClientID:'632633564808-v36dt32qjgee82rs5qtek7qqndc6heht.apps.googleusercontent.com',
+  googleClientSecret:'WP-gUj0GJXCchDC6lo40f8Wa'
+}
